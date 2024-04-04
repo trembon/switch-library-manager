7b28056722d48ecb6d122adf0a12ca2e25dff504
trembon <trembon@hotmail.com>
2024-04-04 20:13:04 +0200

    try to print output and some logic around csv export

--- src/console.go
+++ src/console.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"os"
 	"path"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"syscall"
 
 	"github.com/jedib0t/go-pretty/table"
 	"github.com/schollz/progressbar/v3"
@@ -16,10 +19,22 @@ import (
 	"go.uber.org/zap"
 )
 
+const (
+	ATTACH_PARENT_PROCESS = ^uint32(0) // (DWORD)-1
+)
+
 var (
-	nspFolder   = flag.String("f", "", "path to NSP folder")
-	recursive   = flag.Bool("r", true, "recursively scan sub folders")
-	mode        = flag.String("m", "", "**deprecated**")
+	modkernel32       = syscall.NewLazyDLL("kernel32.dll")
+	procAttachConsole = modkernel32.NewProc("AttachConsole")
+)
+
+func AttachConsole(dwParentProcess uint32) (ok bool) {
+	r0, _, _ := syscall.SyscallN(procAttachConsole.Addr(), 1, uintptr(dwParentProcess), 0, 0)
+	ok = bool(r0 != 0)
+	return
+}
+
+var (
 	progressBar *progressbar.ProgressBar
 )
 
@@ -33,13 +48,44 @@ func CreateConsole(baseFolder string, sugarLogger *zap.SugaredLogger) *Console {
 }
 
 func (c *Console) Start() {
-	flag.Parse()
-
-	if mode != nil && *mode != "" {
-		fmt.Println("note : the mode option ('-m') is deprecated, please use the settings.json to control options.")
+	ok := AttachConsole(ATTACH_PARENT_PROCESS)
+	if ok {
+		fmt.Println("Okay, attached")
 	}
 
+	flagSet := flag.NewFlagSet("console", flag.ContinueOnError)
+
+	nspFolder := flagSet.String("f", "", "path to NSP folder")
+	recursive := flagSet.Bool("r", true, "recursively scan sub folders")
+	exportCsvFolder := flagSet.String("export", "", "path to NSP folder")
+
+	flagSet.Parse(os.Args[1:])
+	fmt.Println("1")
+
+	fmt.Println("2")
+
 	settingsObj := settings.ReadSettings(c.baseFolder)
+
+	fmt.Println("3")
+	csvOutput := *exportCsvFolder
+	fmt.Println("exportCsvFolder", *exportCsvFolder)
+	/*if indexOf(os.Args, "--export-csv") >= 0 {
+		index := indexOf(os.Args, "--export-csv")
+		if len(os.Args) > index+1 && !strings.HasPrefix(os.Args[index+1], "--") {
+			csvOutput = os.Args[index+1]
+		} else {
+			csvOutput = filepath.Join(c.baseFolder, "csv")
+		}
+
+		if _, err := os.Stat(csvOutput); os.IsNotExist(err) {
+			err = os.Mkdir(csvOutput, os.ModePerm)
+			if err != nil {
+				fmt.Printf("Failed to create folder for csv output %v - %v\n", csvOutput, err)
+				zap.S().Errorf("Failed to create folder for csv output %v - %v\n", csvOutput, err)
+			}
+		}
+	}*/
+	fmt.Println("4")
 
 	//1. load the titles JSON object
 	fmt.Printf("Downlading latest switch titles json file")
@@ -136,12 +182,24 @@ func (c *Console) Start() {
 
 	if settingsObj.CheckForMissingUpdates {
 		fmt.Printf("\nChecking for missing updates\n")
-		c.processMissingUpdates(localDB, titlesDB, settingsObj)
+
+		missingUpdatesCsvFile := ""
+		if csvOutput != "" {
+			missingUpdatesCsvFile = filepath.Join(csvOutput, "missing_updates.csv")
+		}
+
+		c.processMissingUpdates(localDB, titlesDB, settingsObj, missingUpdatesCsvFile)
 	}
 
 	if settingsObj.CheckForMissingDLC {
 		fmt.Printf("\nChecking for missing DLC\n")
-		c.processMissingDLC(localDB, titlesDB)
+
+		missingDlcCsvFile := ""
+		if csvOutput != "" {
+			missingDlcCsvFile = filepath.Join(csvOutput, "missing_dlc.csv")
+		}
+
+		c.processMissingDLC(localDB, titlesDB, missingDlcCsvFile)
 	}
 
 	fmt.Printf("Completed")
@@ -166,7 +224,14 @@ func (c *Console) processIssues(localDB *db.LocalSwitchFilesDB) {
 	t.Render()
 }
 
-func (c *Console) processMissingUpdates(localDB *db.LocalSwitchFilesDB, titlesDB *db.SwitchTitlesDB, settingsObj *settings.AppSettings) {
+func (c *Console) processMissingUpdates(localDB *db.LocalSwitchFilesDB, titlesDB *db.SwitchTitlesDB, settingsObj *settings.AppSettings, csvOutput string) {
+	var csvWriter *csv.Writer = nil
+	var csvFile *os.File = nil
+	if csvOutput != "" {
+		csvFile, _ = os.Create(csvOutput)
+		csvWriter = csv.NewWriter(csvFile)
+	}
+
 	incompleteTitles := process.ScanForMissingUpdates(localDB.TitlesMap, titlesDB.TitlesMap, settingsObj.IgnoreDLCUpdates)
 	if len(incompleteTitles) != 0 {
 		fmt.Print("\nFound available updates:\n\n")
@@ -180,14 +245,31 @@ func (c *Console) processMissingUpdates(localDB *db.LocalSwitchFilesDB, titlesDB
 	t.AppendHeader(table.Row{"#", "Title", "TitleId", "Local version", "Latest Version", "Update Date"})
 	i := 0
 	for _, v := range incompleteTitles {
+		if csvWriter != nil {
+			row := []string{v.Attributes.Name, v.Attributes.Id, strconv.Itoa(v.LocalUpdate), strconv.Itoa(v.LatestUpdate), v.LatestUpdateDate}
+			_ = csvWriter.Write(row)
+		}
+
 		t.AppendRow([]interface{}{i, v.Attributes.Name, v.Attributes.Id, v.LocalUpdate, v.LatestUpdate, v.LatestUpdateDate})
 		i++
 	}
 	t.AppendFooter(table.Row{"", "", "", "", "Total", len(incompleteTitles)})
 	t.Render()
+
+	if csvOutput != "" {
+		csvWriter.Flush()
+		csvFile.Close()
+	}
 }
 
-func (c *Console) processMissingDLC(localDB *db.LocalSwitchFilesDB, titlesDB *db.SwitchTitlesDB) {
+func (c *Console) processMissingDLC(localDB *db.LocalSwitchFilesDB, titlesDB *db.SwitchTitlesDB, csvOutput string) {
+	var csvWriter *csv.Writer = nil
+	var csvFile *os.File = nil
+	if csvOutput != "" {
+		csvFile, _ = os.Create(csvOutput)
+		csvWriter = csv.NewWriter(csvFile)
+	}
+
 	settingsObj := settings.ReadSettings(c.baseFolder)
 	ignoreIds := map[string]struct{}{}
 	for _, id := range settingsObj.IgnoreDLCTitleIds {
@@ -206,15 +288,33 @@ func (c *Console) processMissingDLC(localDB *db.LocalSwitchFilesDB, titlesDB *db
 	t.AppendHeader(table.Row{"#", "Title", "TitleId", "Missing DLCs (titleId - Name)"})
 	i := 0
 	for _, v := range incompleteTitles {
+		if csvWriter != nil {
+			row := []string{v.Attributes.Name, v.Attributes.Id, strings.Join(v.MissingDLC, "\n")}
+			_ = csvWriter.Write(row)
+		}
+
 		t.AppendRow([]interface{}{i, v.Attributes.Name, v.Attributes.Id, strings.Join(v.MissingDLC, "\n")})
 		i++
 	}
 	t.AppendFooter(table.Row{"", "", "", "", "Total", len(incompleteTitles)})
 	t.Render()
+
+	if csvOutput != "" {
+		csvWriter.Flush()
+		csvFile.Close()
+	}
 }
 
 func (c *Console) UpdateProgress(curr int, total int, message string) {
 	progressBar.ChangeMax(total)
 	progressBar.Set(curr)
+}
 
+func indexOf(s []string, e string) int {
+	for i, a := range s {
+		if a == e {
+			return i
+		}
+	}
+	return -1
 }
