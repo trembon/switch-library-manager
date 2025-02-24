6236491cf7afcc0e750d242c48de7203417cbf60
trembon <trembon@hotmail.com>
2025-02-24 21:55:20 +0100

    add cloudflare build script

---
+++ .cloudflare/build.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/blawar/nut
+cd nut
+
+pip install -r requirements.txt
+git clone --depth=1 https://github.com/blawar/titledb
+
+nut.py --import-region US --language en
+
+mkdir ../titles_output
+
+cp titledb/titles.json ../titles_output/titles.json
+cp titledb/cnmts.json ../titles_output/cnmts.json
+cp titledb/versions.json ../titles_output/versions.json
+cp titledb/versions.txt ../titles_output/versions.txt
